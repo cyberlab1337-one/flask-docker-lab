734e2012cf4915ac922111b3330f248a5791199a
Monika <236606736+cyberlab1337-one@users.noreply.github.com>
2026-09-21 16:18:57 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Flask Docker Lab
+
+Simple Docker practice project with a Flask application and PostgreSQL.
+
+![Pipeline Stages](images/docker-flask-postgresql2.pngdocker-flask-postgresql2.png)
+
+## Technologies
+
+- Python
+- Flask
+- Docker
+- Docker Compose
+- PostgreSQL
+
+## Actions
+
+- Building Docker images
+- Running containers
+- Port mapping
+- Using Docker Compose
+- Connecting a Flask application to PostgreSQL
+- Inspecting containers and checking logs
+
+## Run the project
+
+```bash
+docker compose up --build
---
+++ app-sql.py
@@ -0,0 +1,71 @@
+from flask import Flask
+import psycopg2
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    conn = psycopg2.connect(
+        host="db",
+        database="mydb",
+        user="myuser",
+        password="mypassword"
+    )
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT 'Hello from PostgreSQL!'")
+    message = cursor.fetchone()[0]
+
+    cursor.close()
+    conn.close()
+
+    return f"""
+    <html>
+        <head>
+            <title>Docker Lab</title>
+            <style>
+                body {{
+                    margin: 0;
+                    height: 100vh;
+                    background-color: #121212;
+                    color: white;
+
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+
+                    font-family: Arial, sans-serif;
+                }}
+
+                h1 {{
+                    font-size: 48px;
+                }}
+
+                .whale {{
+                    font-size: 140px;
+                }}
+
+                .database {{
+                    font-size: 24px;
+                    margin-top: 20px;
+                }}
+            </style>
+        </head>
+
+        <body>
+            <h1>Flask running in Docker</h1>
+
+            <div class="whale">
+                🐳
+            </div>
+
+            <div class="database">
+                {message}
+            </div>
+        </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
